674caee7b6d2396fc0983219624cc6d14b2bcf11
BrWri4227 <122122599+BrWri4227@users.noreply.github.com>
2023-01-16 00:01:01 -0500

    Finished for now, maybe more styling, searchbar?

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -3,19 +3,34 @@ import moment from 'moment';
 
 const Weather = ({ weatherData }) => {
   const [direction, setDir] = useState([]);
+  const [time, setTime] = useState(new Date());
+  const [gust, setGust] = useState([Math.round(weatherData?.wind?.gust * 3.6)]);
   useEffect(() => {
-    const val = Math.round((weatherData?.wind?.deg / 22.5) + 0.5)
+    const val = Math.round((weatherData?.wind?.deg / 45) + 0.5)
     console.log(weatherData?.wind?.deg)
-    const arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]
+    const arr = ["N","NE","E", "SE","S","SW","W","NW"];
     console.log(weatherData?.wind?.deg / 22.5 + 0.5)
     setDir(arr[val % 16])
+    setGust(Math.round(weatherData?.wind?.gust * 3.6))
   }, [weatherData]);
+  useEffect(() => {
+
+    const interval = setInterval(() => {
+      setTime(new Date())
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
 
     <div className="backdrop">
+      <h1></h1>
       <div className="container">
         <div className="location">
           <p>{weatherData?.name}</p>
+          <div className="time">
+          <p>{time.toLocaleTimeString('en-IN')}</p>
+          </div>
         </div>
         <div className="flex">
           <div className='temp'>
@@ -33,19 +48,22 @@ const Weather = ({ weatherData }) => {
             <p>Humidity: {weatherData?.main?.humidity}%</p>
           </div>
         </div>
+        <div className="flex">
         <div className='wind'>
-          {/* <p>Wind: {weatherData?.wind?.speed * 3.6} km/h at {weatherData?.wind?.deg}° with Gusts of: {(weatherData?.wind?.gust * 3.6).toFixed(0)} km/h</p> */}
+          <p>Wind: {(weatherData?.wind?.speed * 3.6).toFixed(0)} km/h <sup style={{fontWeight: "600", fontSize: "0.75rem"}}>{direction}</sup></p>
+        
         </div>
         <div className='gust'>
-          <p>Wind: {(weatherData?.wind?.speed * 3.6).toFixed(0)} km/h at {direction} with Gusts of: {(weatherData?.wind?.gust * 3.6).toFixed(0)} km/h</p>
+          <p>Gust: {gust > 0 ? gust : '0'} km/h</p>
+        </div>
         </div>
         <div className="flex">
-        <div className='sunrise'>
-          <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
-        </div>
-        <div className='sunset'>
-          <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
-        </div>
+          <div className='sunrise'>
+            <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+          </div>
+          <div className='sunset'>
+            <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+          </div>
         </div>
         <div className='date'>
           <p>Date: {moment().format('LL')}</p>
