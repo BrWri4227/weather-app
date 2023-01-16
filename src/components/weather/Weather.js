a51d861c75f4c398fb4fcca2dfb41b6a839c4a3a
BrWri4227 <122122599+BrWri4227@users.noreply.github.com>
2023-01-15 23:25:50 -0500

    intense styling, still need to figure out conditional rendering for gusts, and add search for locations

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,33 +1,37 @@
 import React, { useEffect, useState } from "react";
+import moment from 'moment';
 
-
-const Weather = ({weatherData}) => {
+const Weather = ({ weatherData }) => {
   const [direction, setDir] = useState([]);
   useEffect(() => {
     const val = Math.round((weatherData?.wind?.deg / 22.5) + 0.5)
     console.log(weatherData?.wind?.deg)
-    const arr= ["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"]
-    console.log(weatherData?.wind?.deg /22.5 + 0.5)
+    const arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]
+    console.log(weatherData?.wind?.deg / 22.5 + 0.5)
     setDir(arr[val % 16])
-  },[weatherData]);
+  }, [weatherData]);
   return (
 
-    <div>
+    <div className="backdrop">
       <div className="container">
         <div className="location">
           <p>{weatherData?.name}</p>
         </div>
-        <div className='temp'>
-          <h1>{(weatherData?.main?.temp).toFixed(0)}&deg;C</h1>
+        <div className="flex">
+          <div className='temp'>
+            <h1>{(weatherData?.main?.temp).toFixed(0)}&deg;C</h1>
+          </div>
+          <div className='description'>
+            <p>{weatherData?.weather[0]?.description}</p>
+          </div>
         </div>
-        <div className='description'>
-          <p>{weatherData?.weather[0]?.description}</p>
-        </div>
-        <div className='feelsLike'>
-          <p>Feels Like: {(weatherData?.main?.feels_like).toFixed(0)}&deg;C</p>
-        </div>
-        <div className='humidity'>
-          <p>Humidity: {weatherData?.main?.humidity}%</p>
+        <div className="flex">
+          <div className='feelsLike'>
+            <p>Feels Like: {(weatherData?.main?.feels_like).toFixed(0)}&deg;C</p>
+          </div>
+          <div className='humidity'>
+            <p>Humidity: {weatherData?.main?.humidity}%</p>
+          </div>
         </div>
         <div className='wind'>
           {/* <p>Wind: {weatherData?.wind?.speed * 3.6} km/h at {weatherData?.wind?.deg}° with Gusts of: {(weatherData?.wind?.gust * 3.6).toFixed(0)} km/h</p> */}
@@ -35,8 +39,19 @@ const Weather = ({weatherData}) => {
         <div className='gust'>
           <p>Wind: {(weatherData?.wind?.speed * 3.6).toFixed(0)} km/h at {direction} with Gusts of: {(weatherData?.wind?.gust * 3.6).toFixed(0)} km/h</p>
         </div>
+        <div className="flex">
+        <div className='sunrise'>
+          <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+        </div>
+        <div className='sunset'>
+          <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+        </div>
+        </div>
+        <div className='date'>
+          <p>Date: {moment().format('LL')}</p>
+        </div>
       </div>
-    </div>
+    </div >
   )
 }
 
